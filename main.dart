ea0752f41fb75dc9cf8cd66ac36f8de57063b326
TukXplorer <dev.tuk01@gmail.com>
2023-06-14 06:24:16 +0100

    variable

--- main.dart
+++ main.dart
@@ -1,7 +1,25 @@
-main(){
-  var firstName = 'taoufik';
-  String lastName = 'ben lhosni';
-  print(firstName + ' ' +lastName);
+main() {
+  //Variable
 
- }
+  // Strings
+  // var name = 'taufik';
+  String name = 'taoufik';
+  print(name);
+  //var x = 41;
+  int x = 41;
+  print(x);
 
+  //Dynamic
+  dynamic firstName = 'ben';
+
+  // Const and Final
+  const String fullName = "taoufik ben"; // compile time
+  final String nickName = 'Tuk'; //run time
+
+  // Declaring and not using
+  var myName;
+  print(myName); //return null
+
+  myName = 'taoufik';
+  print(myName);
+}
