75e65090798e340a2777ef9d1666604aea5ad911
TukXplorer <dev.tuk01@gmail.com>
2023-06-16 19:18:12 +0100

    Functions

--- main.dart
+++ main.dart
@@ -1,17 +1,43 @@
-// Logic
+// Function
 void main() {
-  // if, if/else, if else if
-  // > < >= <= !=
-  var num = 23;
-  if (num == 5) {
-    print('thes number is $num');
-  } 
-  else if (num == 12) {
-    print('the numbre is $num');
-  } else {
-    print('the numbre is out');
+  // Example one
+  myFunc() {
+    return 'Hello Functions';
   }
 
-  
+  var thing = myFunc();
+  print(thing);
+
+  // Example with argument 
+  myFunc2(String name) {
+    return 'Hello $name';
+  }
+
+  var thing2 = myFunc2('Tuk');
+  print(thing2);
+
+  // Example with more than one argument
+  myFunc3(String name1, name2 ) {
+    return 'Hello $name1 and $name2';
+  }
+
+  var thing3 = myFunc3('Tuk', 'SS');
+  print(thing3);
+
+  // example with optional
+  myFunc4(String name1, [name2] ) {
+    return 'Hello $name1 and $name2';
+  }
+
+  var thing4 = myFunc4('Tuk');
+  print(thing4);
+
+  // ex
+  myFunc5(String name1, {name2="friends"}) {
+    return 'Hello $name1 and $name2';
+  }
+
+  var thing5 = myFunc5('Tuk',name2:"Bill" );
+  print(thing5);
 
 }
