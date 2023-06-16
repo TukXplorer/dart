2e5d8d5712362c07bc8817b57661a81db1ad23e9
TukXplorer <dev.tuk01@gmail.com>
2023-06-16 21:12:20 +0100

    convert str>int

--- main.dart
+++ main.dart
@@ -1,13 +1,24 @@
-// User input
-import 'dart:io';
-
+// Convert String to integer
 void main() {
-  //
-  print('Enter your name:');
-  // Allow the user to input their name
-  var name = stdin.readLineSync();
-  print('Hello $name');
+  //string to int
+  var a, b, c;
+  a = 40;
+  b = "1";
+  c = a + int.parse(b);
+  print('$a + $b = $c');
 
-  String? name2 = stdin.readLineSync();
-  print('Hello $name2');
+  // string to double
+  var d, e, f;
+  d = 40;
+  e = '0.1';
+  f = d + double.parse(e);
+  print('$d + $e = $f');
+
+  // int or double to string
+  var g, h, i;
+  g = 40;
+  h = '1';
+  i = g.toString() + h;
+  print('$g + $h = $i');
+
 }
