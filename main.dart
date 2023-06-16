4ee349753f638e3b40b311f4c50eee7d59c64263
TukXplorer <dev.tuk01@gmail.com>
2023-06-16 20:57:27 +0100

    user imput

--- main.dart
+++ main.dart
@@ -1,43 +1,13 @@
-// Function
+// User input
+import 'dart:io';
+
 void main() {
-  // Example one
-  myFunc() {
-    return 'Hello Functions';
-  }
+  //
+  print('Enter your name:');
+  // Allow the user to input their name
+  var name = stdin.readLineSync();
+  print('Hello $name');
 
-  var thing = myFunc();
-  print(thing);
-
-  // Example with argument 
-  myFunc2(String name) {
-    return 'Hello $name';
-  }
-
-  var thing2 = myFunc2('Tuk');
-  print(thing2);
-
-  // Example with more than one argument
-  myFunc3(String name1, name2 ) {
-    return 'Hello $name1 and $name2';
-  }
-
-  var thing3 = myFunc3('Tuk', 'SS');
-  print(thing3);
-
-  // example with optional
-  myFunc4(String name1, [name2] ) {
-    return 'Hello $name1 and $name2';
-  }
-
-  var thing4 = myFunc4('Tuk');
-  print(thing4);
-
-  // ex
-  myFunc5(String name1, {name2="friends"}) {
-    return 'Hello $name1 and $name2';
-  }
-
-  var thing5 = myFunc5('Tuk',name2:"Bill" );
-  print(thing5);
-
+  String? name2 = stdin.readLineSync();
+  print('Hello $name2');
 }
