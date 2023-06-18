02443b1ad913fc984ff3fc6b361de51fd6512d5b
TukXplorer <dev.tuk01@gmail.com>
2023-06-18 21:10:18 +0100

    class & OOP

--- main.dart
+++ main.dart
@@ -1,20 +1,30 @@
-// Fizzbuzz
+// Class and OOP
 
 void main() {
-  //loop
-  int num = 1;
-  while (num <= 100) {
-    if (num % 5 == 0 && num % 3 == 0) {
-      print('$num. Fuzzbuzz!');
-    } else if (num % 3 == 0) {
-      print('$num. Fizz');
-    } else if (num % 5 == 0) {
-      print('$num. buzz');
-    } else {
-      print('$num.');
-    }
+  // Create Person Class
+  Person p1 = Person('John', 'Male', 41);
+  p1.showData();
+  print(p1.name);
 
-    // Increment the counter
-    num++;
+  Person p2 = Person('Ven', 'Male', 1200);
+  p2.showData();
+}
+
+class Person {
+  String? name, sex;
+  int? age;
+
+  // Costructeur
+  Person(String name, sex, int age) {
+    this.name = name;
+    this.sex = sex;
+    this.age = age;
+  }
+  // Method
+  void showData() {
+    print('Name = $name');
+    print('Sex = $sex');
+    print('age = $age');
+    print('the person name is $name, they are $sex, and $age years old');
   }
 }
