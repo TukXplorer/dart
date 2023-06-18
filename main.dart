ed401e155170c390eca5e9ac7d39593f0f175954
TukXplorer <dev.tuk01@gmail.com>
2023-06-18 20:24:38 +0100

    convert from user

--- main.dart
+++ main.dart
@@ -1,24 +1,9 @@
-// Convert String to integer
+// Type convertion from user
+import 'dart:io';
+
 void main() {
-  //string to int
-  var a, b, c;
-  a = 40;
-  b = "1";
-  c = a + int.parse(b);
-  print('$a + $b = $c');
-
-  // string to double
-  var d, e, f;
-  d = 40;
-  e = '0.1';
-  f = d + double.parse(e);
-  print('$d + $e = $f');
-
-  // int or double to string
-  var g, h, i;
-  g = 40;
-  h = '1';
-  i = g.toString() + h;
-  print('$g + $h = $i');
-
+  print('Enter A number:');
+  var num = stdin.readLineSync();
+  var num2 = int.parse(num ?? "0") + 10;
+  print(num2);
 }
